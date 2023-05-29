0c9b56fe71e197cffa9176176cbbb944f6627b99
kelvao <kelvintesche@gmail.com>
2023-05-29 18:24:39 -0300

    Feat: fixed issues with version catalog and added main activity

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed (https://youtrack.jetbrains.com/issue/KTIJ-19369)
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+//TODO: Workaround for "Expecting an expression" build error from plugin, remove once fixed
+println("")
